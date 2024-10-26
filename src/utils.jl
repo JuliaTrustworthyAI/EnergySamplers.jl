9a1d9495ca4fbeb3b0abf15fda2fda5ac7f43c38
pat-alt <altmeyerpat@gmail.com>
2024-10-26 11:42:22 +0200

    energy differential and energy penalty added

--- src/utils.jl
+++ src/utils.jl
@@ -1,12 +1,17 @@
 using Flux
 using StatsBase
 
+"""
+    get_logits(f::Flux.Chain, x)
+
+Retrieves the logits (linear predictions) of a `Chain` for the input `x`.
+"""
 get_logits(f::Flux.Chain, x) = f[end] isa Function ? f[1:(end - 1)](x) : f(x)
 
 @doc raw"""
-    energy(f, x)
+    _energy(f, x; agg=mean)
 
-Computes the energy for unconditional samples $x \sim p_{\theta}(x)$: $E(x)=-\text{LogSumExp}_y f_{\theta}(x)[y]$.
+Computes the energy for unconditional samples $x \sim p_{\theta}(x)$: $E(x)=-\text{LogSumExp}_y f_{\theta}(x)[y]$. $DOC_Grathwohl
 """
 function _energy(f, x; agg=mean)
     if f isa Flux.Chain
@@ -24,9 +29,9 @@ function _energy(f, x; agg=mean)
 end
 
 @doc raw"""
-    energy(f, x, y::Int; agg=mean)
+    _energy(f, x, y::Int; agg=mean)
 
-Computes the energy for conditional samples $x \sim p_{\theta}(x|y)$: $E(x)=- f_{\theta}(x)[y]$.
+Computes the energy for conditional samples $x \sim p_{\theta}(x|y)$: $E(x)=- f_{\theta}(x)[y]$. $DOC_Grathwohl
 """
 function _energy(f, x, y::Int; agg=mean)
     if f isa Flux.Chain
@@ -40,6 +45,30 @@ function _energy(f, x, y::Int; agg=mean)
         E = agg(map(_y -> _E(_y, y), eachslice(ŷ; dims=ndims(ŷ))))
         return E
     else
-        return _E(_y, y)
+        return _E(ŷ, y)
     end
+end
+
+@doc raw"""
+    energy_differential(f, xgen, xsampled, y::Int; agg=mean)
+
+Computes the energy differential between a conditional sample ``x_{\text{gen}} \sim p_{\theta}(x|y)`` and an observed sample ``x_{\text{sample}} \sim p(x|y)`` as ``E(x_{\text{sample}}|y) - E(x_{\text{gen}}|y)`` with ``E(x|y) = -f_{\theta}(x)[y]``. $DOC_Grathwohl
+"""
+function energy_differential(f, xgen, xsampled, y::Int; agg=mean)
+    neg_loss = _energy(f, xgen, y; agg=agg)         # negative loss associated with generated samples
+    pos_loss = _energy(f, xsampled, y; agg=agg)     # positive loss associated with sampled samples
+    ℓ = pos_loss - neg_loss
+    return ℓ
+end
+
+@doc raw"""
+    energy_penalty(f, xgen, xsampled, y::Int; agg=mean)
+
+Computes the a Ridge penalty for the overall energies of the conditional samples ``x_{\text{gen}} \sim p_{\theta}(x|y)`` and an observed sample ``x_{\text{sample}} \sim p(x|y)``. $DOC_Grathwohl
+"""
+function energy_penalty(f, xgen, xsampled, y::Int; agg=mean, p=1)
+    neg_loss = _energy(f, xgen, y; agg=agg)         # negative loss associated with generated samples
+    pos_loss = _energy(f, xsampled, y; agg=agg)     # positive loss associated with sampled samples
+    ℓ = neg_loss .^ 2 .+ pos_loss .^ 2
+    return ℓ
 end
